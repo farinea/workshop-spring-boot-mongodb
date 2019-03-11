97592879947d1aa4504bd49ba5578ddcdf834e41
Wagner Farinea <wagnereffe@gmail.com>
2019-03-11 12:32:59 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/farinea/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.farinea.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.farinea.workshopmongo.domain.User;
+import com.farinea.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
